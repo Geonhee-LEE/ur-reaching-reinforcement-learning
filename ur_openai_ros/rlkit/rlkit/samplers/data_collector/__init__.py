b7278293b1642f36fb5dd99ad67158c4d45f4a8c
Geonhee-LEE <gunhee6392@gmail.com>
2019-10-11 10:09:50 +0900

    Modify the rlkit for debug

---
+++ ur_openai_ros/rlkit/rlkit/samplers/data_collector/__init__.py
@@ -0,0 +1,12 @@
+from rlkit.samplers.data_collector.base import (
+    DataCollector,
+    PathCollector,
+    StepCollector,
+)
+from rlkit.samplers.data_collector.path_collector import (
+    MdpPathCollector,
+    GoalConditionedPathCollector,
+)
+from rlkit.samplers.data_collector.step_collector import (
+    GoalConditionedStepCollector
+)
